29bffbef74325590e49ffaaddc1556b87f2fe97b
Windson yang <wiwindson@outlook.com>
2018-10-08 23:07:23 +0800

    add resources models and migrations

--- Paas/resources/models.py
+++ Paas/resources/models.py
@@ -1,3 +1,9 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Resource(models.Model):
+    unique_id = models.UUIDField(default=uuid.uuid4, editable=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
