bf278309538dd9b53fd2a1bb0ed96636bfa7bd89
Windson yang <wiwindson@outlook.com>
2018-10-10 13:12:55 +0800

    override serializers create method

--- Paas/resources/serializers.py
+++ Paas/resources/serializers.py
@@ -6,3 +6,8 @@ class ResourceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Resource
         fields = ('id', 'unique_id', 'create_time')
+
+    def create(self, validated_data):
+        user =  self.context['request'].user
+        validated_data['owner'] = user
+        return Resource.objects.create(**validated_data)
