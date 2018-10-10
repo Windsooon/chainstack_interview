f48b456a829cb5eb2bcbf5aa4c9e496e4ea28fed
Windson yang <wiwindson@outlook.com>
2018-10-10 17:06:08 +0800

    superuser can operate all resources

--- Paas/resources/permissions.py
+++ Paas/resources/permissions.py
@@ -4,4 +4,4 @@ class IsOwner(permissions.BasePermission):
     message = 'Only owner can access the resource.'
 
     def has_object_permission(self, request, view, obj):
-        return request.user and request.user.is_authenticated and obj.owner == request.user
+        return request.user and request.user.is_authenticated and (obj.owner == request.user) or request.user.is_admin
