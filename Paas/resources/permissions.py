58201ef3d1359dde4d368de453682839a8863221
Windson yang <wiwindson@outlook.com>
2018-10-10 17:43:09 +0800

    pretect unau user access resource

--- Paas/resources/permissions.py
+++ Paas/resources/permissions.py
@@ -3,5 +3,8 @@ from rest_framework import permissions
 class IsOwner(permissions.BasePermission):
     message = 'Only owner can access the resource.'
 
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
         return request.user and request.user.is_authenticated and (obj.owner == request.user) or request.user.is_admin
