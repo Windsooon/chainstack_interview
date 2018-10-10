bd599d5be7a6e67603b6941c93e6fa1947ce20f3
Windson yang <wiwindson@outlook.com>
2018-10-10 14:34:33 +0800

    add api test

---
+++ Paas/runtests.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import os
+import sys
+import django
+from django.conf import settings
+from django.test.utils import get_runner
+
+if __name__ == "__main__":
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'Paas.settings'
+    settings.PASSWORD_HASHERS = [
+            'django.contrib.auth.hashers.MD5PasswordHasher',
+    ]
+    settings.DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'db.sqlite3',
+        }
+    }
+    settings.MIDDLEWARE_CLASSES = [
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+    ]
+    django.setup()
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner()
+    failures = test_runner.run_tests(["tests"])
+    sys.exit(bool(failures))
---
+++ Paas/tests/test_api.py
@@ -0,0 +1,34 @@
+import json
+from django.urls import reverse
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from resources.models import Resource
+
+
+class ApiTestCase(TestCase):
+
+    def test_unlimited_user_create_resource(self):
+        self.user = get_user_model().objects.create_user(
+            email='test@test.com', password='test_password')
+        self.assertEqual(0, Resource.objects.count())
+        self.client.force_login(self.user)
+        response = self.client.post(
+            '/resource/', content_type='application/json')
+        self.assertEqual(1, Resource.objects.count())
+        response = self.client.post(
+            '/resource/', content_type='application/json')
+        self.assertEqual(2, Resource.objects.count())
+
+    def test_limited_user_create_resource(self):
+        self.user = get_user_model().objects.create_user(
+            email='test@test.com', quota=1, password='test_password')
+        self.assertEqual(0, Resource.objects.count())
+        self.client.force_login(self.user)
+        response = self.client.post(
+            '/resource/', content_type='application/json')
+        self.assertEqual(1, Resource.objects.count())
+        response = self.client.post(
+            '/resource/', content_type='application/json')
+        self.assertEqual(1, Resource.objects.count())
+        self.assertEqual(response.content, b'["You do not have enough quota."]')
+
