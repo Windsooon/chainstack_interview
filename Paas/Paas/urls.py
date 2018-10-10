355dd5bd25889b5738ab05636b403b87100499e4
Windson yang <wiwindson@outlook.com>
2018-10-10 18:37:37 +0800

    add API document

--- Paas/Paas/urls.py
+++ Paas/Paas/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from django.urls import path
 from rest_framework import routers
+from rest_framework.documentation import include_docs_urls
 from resources import views
 
 router = routers.DefaultRouter()
@@ -9,6 +10,7 @@ router.register(r'resource', views.ResourceViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^docs/', include_docs_urls(title='API Docs')),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
